1b36870b5296b763352d859d27ee38c8e69e60e7
정우용 <wooyong.dev@gmail.com>
2024-07-30 21:51:36 +0900

    Revert "Dev/feat/phone number"

--- src/main/java/com/innercircle/validation/ValidatorManager.java
+++ src/main/java/com/innercircle/validation/ValidatorManager.java
@@ -1,10 +1,8 @@
 package com.innercircle.validation;
 
 import com.innercircle.validation.annotation.Email;
-import com.innercircle.validation.annotation.PhoneNumber;
 import com.innercircle.validation.annotation.ResidentRegistrationNumber;
 import com.innercircle.validation.validator.EmailValidator;
-import com.innercircle.validation.validator.PhoneNumberValidator;
 import com.innercircle.validation.validator.ResidentRegistrationNumberValidator;
 import com.innercircle.validation.validator.Validator;
 
@@ -18,7 +16,6 @@ public class ValidatorManager {
 
     static {
         validators.put(Email.class, new EmailValidator());
-        validators.put(PhoneNumber.class, new PhoneNumberValidator());
         validators.put(ResidentRegistrationNumber.class, new ResidentRegistrationNumberValidator());
     }
 

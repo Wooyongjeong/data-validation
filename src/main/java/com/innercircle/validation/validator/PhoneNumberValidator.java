87fbf44e5158427ee413ca8987e3fb78fb60c4d7
LimHyungang <hyungang7@gmail.com>
2024-07-31 20:52:44 +0900

    [24.07.31] 패턴을 미리 생성하여 패턴 컴파일을 1회만 하도록 변경

--- src/main/java/com/innercircle/validation/validator/PhoneNumberValidator.java
+++ src/main/java/com/innercircle/validation/validator/PhoneNumberValidator.java
@@ -7,17 +7,20 @@ import java.lang.reflect.Field;
 import java.util.regex.Pattern;
 
 public class PhoneNumberValidator implements Validator {
+    Pattern pattern = null;
     @Override
     public void validate(Field field, Object obj) throws ValidatorException, IllegalAccessException {
         PhoneNumber annotation = field.getAnnotation(PhoneNumber.class);
+        if(this.pattern == null) {
+            pattern = Pattern.compile("^(01[016789]-\\d{3,4}-\\d{4}|01[016789]\\d{7,8})$");
+        }
 
         Object value =  field.get(obj);
         if(!(value instanceof String)) {
             throw new ValidatorException(annotation.invalidFieldTypeMessage());
         }
 
-        String phoneNumberPattern = "^(01[016789]-\\d{3,4}-\\d{4}|01[016789]\\d{7,8})$";
-        if (!Pattern.matches(phoneNumberPattern, (String)value)) {
+        if (!pattern.matcher((String)value).matches()) {
             throw new ValidatorException(annotation.invalidFormatMessage());
         }
     }

42d395d64ac402b6c0fe914a9e4ee04dc8dac9ca
LimHyungang <hyungang7@gmail.com>
2024-08-01 19:04:02 +0900

    [24.07.31] 패턴 객체 static 으로 변경

--- src/main/java/com/innercircle/validation/validator/PhoneNumberValidator.java
+++ src/main/java/com/innercircle/validation/validator/PhoneNumberValidator.java
@@ -7,11 +7,11 @@ import java.lang.reflect.Field;
 import java.util.regex.Pattern;
 
 public class PhoneNumberValidator implements Validator {
-    Pattern pattern = null;
+    private static Pattern pattern = null;
     @Override
     public void validate(Field field, Object obj) throws ValidatorException, IllegalAccessException {
         PhoneNumber annotation = field.getAnnotation(PhoneNumber.class);
-        if(this.pattern == null) {
+        if(pattern == null) {
             pattern = Pattern.compile("^(01[016789]-\\d{3,4}-\\d{4}|01[016789]\\d{7,8})$");
         }
 

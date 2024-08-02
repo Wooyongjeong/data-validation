89e62e6fbd849de30d8af6d124ca5e8771a29c74
LimHyungang <hyungang7@gmail.com>
2024-08-02 21:08:48 +0900

    [24.08.02] credit card number validator

---
+++ src/main/java/com/innercircle/validation/validator/CreditCardNumberValidator.java
@@ -0,0 +1,20 @@
+package com.innercircle.validation.validator;
+
+import com.innercircle.validation.annotation.CreditCardNumber;
+import com.innercircle.validation.exception.ValidatorException;
+import com.innercircle.validation.util.StringValidationUtil;
+
+import java.lang.reflect.Field;
+import java.util.regex.Pattern;
+
+public class CreditCardNumberValidator implements Validator {
+    private static Pattern pattern = null;
+    @Override
+    public void validate(Field field, Object obj) throws ValidatorException, IllegalAccessException {
+        CreditCardNumber annotation = field.getAnnotation(CreditCardNumber.class);
+        if(pattern == null) {
+            pattern = Pattern.compile("^(\\d{4}-\\d{4}-\\d{4}-\\d{4}|\\d{16})$");
+        }
+        StringValidationUtil.validate(field, obj, pattern, "");
+    }
+}

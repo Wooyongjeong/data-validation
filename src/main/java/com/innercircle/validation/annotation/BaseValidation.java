a142ccb51be9a9d0c41b0393e6939f6fa129f955
wooyongjeong <wooyong.dev@gmail.com>
2024-08-06 21:26:12 +0900

    feat(BaseValidation): Update BaseValidation
    delete @Inherited

--- src/main/java/com/innercircle/validation/annotation/BaseValidation.java
+++ src/main/java/com/innercircle/validation/annotation/BaseValidation.java
@@ -1,8 +1,5 @@
 package com.innercircle.validation.annotation;
 
-import java.lang.annotation.Inherited;
-
-@Inherited
 public @interface BaseValidation {
     String pattern() default "";
     String message() default "Invalid format";
